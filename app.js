8e181cfacabcae5345e66ddd627cb87090d5f0f5
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-11-20 01:09:24 +0400

    add API

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// express app
-
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
@@ -8,7 +6,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -17,7 +14,6 @@ const connection = mysql.createConnection({
     port: 3306
 });
 
-// connect to database
 connection.connect(function (err) {
     if (err) throw err;
     console.log('You are now connected...');
@@ -33,7 +29,6 @@ app.post('/', function (req, res) {
     const sql = 'INSERT INTO baited (email, password) VALUES (?, ?)';
     connection.query(sql, [email, password]);
 
-    // sql injection vulnerable query
     const sql2 = "SELECT * FROM users WHERE email = '" + email + "' AND password = '" + password + "'";
     console.log(sql2);
     connection.query(sql2, function (err, result) {
@@ -44,6 +39,20 @@ app.post('/', function (req, res) {
             message = "Login failed";
         }
         res.render('index', { message: message, result: result });
+    });
+});
+
+app.post('/api/login', function (req, res) {
+    const password = req.body.password;
+    const email = req.body.email;
+    const sql = "SELECT * FROM users WHERE email = '" + email + "' AND password = '" + password + "'";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.send(200);
+        } else {
+            res.send(401);
+        }
     });
 });
 
